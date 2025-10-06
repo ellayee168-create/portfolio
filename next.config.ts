5813c4f0d550418149cd0bc59b1d62e9940490d7
Ella Yee <ellayee@MacBook-Pro-1355.local>
2025-10-06 19:38:41 -0400

    Fixing redirect

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,15 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
+        // Only redirect when visiting the wrong domain
+        has: [
+          {
+            type: "host",
+            value: "https://portfolio-4pwx-mfq1kw7fj-ellayee168-creates-projects.vercel.app/",
+          },
+        ],
         source: "/:path*",
-        destination: "https://portfolio-4pwx.vercel.app/", // ← replace this
+        destination: "https://portfolio-4pwx.vercel.app/",
         permanent: true,
       },
     ];
