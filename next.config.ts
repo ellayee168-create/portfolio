b204924f54eb24b8f9e317d253d5710b0870b520
Ella Yee <ellayee@MacBook-Pro-1355.local>
2025-10-06 19:34:22 -0400

    Add redirect to public link

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://portfolio-4pwx.vercel.app/", // ← replace this
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
